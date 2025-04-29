ad1c134afe522288cf7ecad1a4312a97fb9733ba
dinoDanic <dino.danic@gmail.com>
2025-04-29 18:55:57 +0200

    colors

--- lua/custom/colors.lua
+++ lua/custom/colors.lua
@@ -1,2 +1,18 @@
-vim.cmd [[ colorscheme rose-pine-main ]]
--- vim.cmd [[ colorscheme tokyonight ]]
+-- Set the colorscheme
+vim.cmd [[colorscheme rose-pine-main]]
+
+require('rose-pine').setup {
+  disable_background = true,
+  disable_float_background = true,
+}
+vim.cmd [[colorscheme rose-pine-main]]
+
+-- Make background transparent
+vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'VertSplit', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'SignColumn', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'StatusLine', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'TabLineFill', { bg = 'none' })
